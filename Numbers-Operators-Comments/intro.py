53832faf4e57bf1fa5496feefab190aaf0884051
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-02 12:23:43 -0800

    Reviewed numbers and operators using python.

---
+++ Numbers-Operators-Comments/intro.py
@@ -0,0 +1,21 @@
+myNumber = 7
+print(myNumber)
+
+myFloat = 7.7
+print(myFloat)
+
+myNumberAddition = 1 + 3
+print(myNumberAddition)
+
+myFloatAddition = 1 + 2.0
+print(myFloatAddition)
+
+mySubtraction = 1 - 3
+print(mySubtraction)
+
+myDivision = 1 / 3
+print(myDivision)
+
+myPower = 2 ** 3
+print(myPower)
+
