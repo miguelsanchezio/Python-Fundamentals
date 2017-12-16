d3f3d0d3943f67ba9eeb264fd734c5861152feb9
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-16 12:12:12 -0800

    Learned about dynamic typing in Python.

--- Variables-And-Strings/variables.py
+++ Variables-And-Strings/variables.py
@@ -8,3 +8,16 @@ CATS != cats # variables are case sensitive
 snake_case = 99 # naming convention is snake case
 SNAKE_CASE_CONST = 99# naming convention for constants is all uppercase
 __double_underscore__ = 99# dunders are supposed to be private or left alone
+
+# variables are dynamic
+dynamic_variable = 10
+print(dynamic_variable)
+
+dynamic_variable = '10X Rule'
+print(dynamic_variable)
+
+dynamic_variable = [1, 10, 100, 1000, 10000]
+print(dynamic_variable)
+
+dynamic_variable = { '10X': 'Rule' }
+print(dynamic_variable)
