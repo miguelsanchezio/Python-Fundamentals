029d179e31a99471bd2f0c66e82851fbce0470b3
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-16 12:12:12 -0800

    Learned about double vs single quotes in Python.

---
+++ strings.py
@@ -0,0 +1,4 @@
+# strings can be declared using single or double quotes
+python_str = 'Single Quotes'
+pythong_other_str = "Double Quotes"
+python_quotes_in_quotes = "Quotes can be 'used' together."
