e6ca48b85bfd38160a62d4eb687739768eeca317
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-16 12:12:12 -0800

    Learned about escape sequences in Python.

--- strings.py
+++ strings.py
@@ -2,3 +2,13 @@
 python_str = 'Single Quotes'
 pythong_other_str = "Double Quotes"
 python_quotes_in_quotes = "Quotes can be 'used' together."
+
+# string escape characters / escape sequences
+new_line = 'Hello \n world'
+print(new_line)
+
+backslash = 'Hello \\ backslash'
+print(backslash)
+
+quotes = 'Hello \'world\''
+print(quotes)
