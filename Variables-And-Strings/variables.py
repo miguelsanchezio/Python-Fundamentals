788861dc2bca2d628f055493d08671408675a31c
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-16 12:12:12 -0800

    Learned about variables in Python.

---
+++ Variables-And-Strings/variables.py
@@ -0,0 +1,10 @@
+num_of_cats = 99 # variable
+all, at, once = 5, 10, 15 # multiple variables
+
+CATS, cats = 7, 7
+
+CATS != cats # variables are case sensitive
+
+snake_case = 99 # naming convention is snake case
+SNAKE_CASE_CONST = 99# naming convention for constants is all uppercase
+__double_underscore__ = 99# dunders are supposed to be private or left alone
