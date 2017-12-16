e618d078c1d21b37029cb2636057c16a719d89f7
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-16 12:12:12 -0800

    Learned about data types.

---
+++ Variables-And-Strings/datatypes.py
@@ -0,0 +1,18 @@
+# booleans
+python_true = True
+python_false = False
+
+# integers
+python_number = 10
+
+# floats
+python_float = 777.777
+
+# strings
+python_string = 'This is a string.'
+
+# list
+python_list = [1,2,3, 'a', 'b', 'c']
+
+#dictionary
+python_dictionary = { 'first_name': 'Miguel', 'last_name': 'Sanchez' }
