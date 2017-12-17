cac8c8b661859563d7c315ee23f7d9dfc1e299f1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-17 12:23:53 -0800

    Learned about conditionals in Python.

---
+++ Booleans-And-Conditional-Logic/conditionals.py
@@ -0,0 +1,16 @@
+# conditional statements
+# if some condition is True:
+#   do something
+# elif some other condition is True:
+#   do something
+# else:
+#   do something
+
+name = 'Grant'
+
+if name == 'Ash Ketchup':
+  print('Pokemon')
+elif name == 'Goku':
+  print('Dragonball')
+else:
+  print('10X')
