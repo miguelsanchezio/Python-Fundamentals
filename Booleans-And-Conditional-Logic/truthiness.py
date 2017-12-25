3f0c404f65cf06c92ec2aa6e53b46e0aa9add7e0
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-25 12:23:51 -0800

    Learned about truthy and falsey values in Python.

---
+++ Booleans-And-Conditional-Logic/truthiness.py
@@ -0,0 +1,12 @@
+# truthiness
+x = 1
+x is 1 # True
+x is 0 # False
+
+animal = input('Enter your favorite animal: ')
+
+if animal:
+  print(animal + ' is cool.')
+else:
+  print('You must pick a favorite animal.')
+
