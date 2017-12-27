8c6b9139787185da57dea330ff472714e5ad9bb8
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-27 15:23:51 -0800

    Learned about comparison operators in Python.

--- Booleans-And-Conditional-Logic/conditionals.py
+++ Booleans-And-Conditional-Logic/conditionals.py
@@ -14,3 +14,11 @@ elif name == 'Goku':
   print('Dragonball')
 else:
   print('10X')
+
+# comparison operators
+1 == 1 # True
+1 != 1 # False
+1 < 2 # True
+1 > 2 # False
+1 <= 1 # True
+1 >= 2 # False
