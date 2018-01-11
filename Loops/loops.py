56bce75def6430aea17aa055194faf6047b3d298
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-11 12:42:12 -0800

    Learned about while loops in Python.

--- Loops/loops.py
+++ Loops/loops.py
@@ -8,3 +8,20 @@ for letter in 'coffee':
 # ranges
 nums = range(10, 100, 5)
 print(list(nums))
+
+# while loops
+msg = input('What\'s the secret password? ')
+while msg != '10X':
+  print('Nope.')
+  msg = input('So, what\'s the secret password? ')
+print('Correct!')
+
+# converting for loop to while loop
+
+for num in range(1, 11):
+    print(num)
+
+num = 1
+while num < 11:
+  print(num)
+  num += 1
