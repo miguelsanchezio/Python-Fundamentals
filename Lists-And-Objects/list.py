aa6c52705ce19049ede09943284dfd7cba488fcc
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-17 12:12:41 -0800

    Learned about the append, insert, and extend methods.

--- Lists-And-Objects/list.py
+++ Lists-And-Objects/list.py
@@ -14,15 +14,22 @@ colors = ['red', 'blue', 'green']
 
 # iterating over lists
 colors_2 = ['orange', 'violet', 'teal']
-for color in colors:
-    print(color)
+# for color in colors:
+#     print(color)
 
 numbers = [4, 21, 412, 41, 15]
-for num in numbers:
-    print(num * num)
+# for num in numbers:
+#     print(num * num)
 
 colors_3 = ['magenta', 'crimson', 'emerald', 'ruby']
 i = 0
-while i < len(colors_3):
-    print(f'{i}: {colors_3[i]}')
-    i += 1
+# while i < len(colors_3):
+#     print(f'{i}: {colors_3[i]}')
+#     i += 1
+
+# list methods
+# append
+data = [1, 2, 3]
+data.append(4)
+data.extend([5, 6, 7, 8])
+data.insert(0, 0)
