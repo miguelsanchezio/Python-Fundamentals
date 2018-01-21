b95977f1a7e2610300d64a2f6324ace08a0766d3
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-21 07:53:53 -0800

    Learned about list comprehension with conditional logic.

--- Lists-And-Objects/list_comprehension.py
+++ Lists-And-Objects/list_comprehension.py
@@ -1,3 +1,6 @@
 numbers = [1, 2, 3, 4, 5]
 string_list = [str(num) for num in numbers]
 print(string_list) # ['1', '2', '3', '4', '5']
+
+numbers_2 = [1, 2, 3, 4, 5, 6]
+evens = [num for num in numbers if num % 2 == 0]
