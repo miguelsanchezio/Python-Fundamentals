204201144a6488c1f002e23c22c4852d639544b8
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-22 12:53:12 -0800

    Addex example for nested lists.

--- Lists-And-Objects/nested_lists.py
+++ Lists-And-Objects/nested_lists.py
@@ -10,3 +10,7 @@ coords = [[10.412, 8.1231], [31.1233, -13.41212], [21.34123, 31.124123]]
 
 [['L' if num % 2 != 0 else 'O' for num in range(1, 4)] for val in range(1, 4)]
 # [['L', 'O', 'L'], ['L', 'O', 'L'], ['L', 'O', 'L']]
+
+answer = [[num for num in range(0, 3)] for val in range(1, 4)]
+
+answer = [[num for num in range(0, 10)] for val in range(0, 10)]
