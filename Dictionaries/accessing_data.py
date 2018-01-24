97f46c984bf2a72b796446751a4d2f6f6a3b219b
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-24 11:53:11 -0800

    Practiced accessing data from dictionaries in Python.

--- Dictionaries/accessing_data.py
+++ Dictionaries/accessing_data.py
@@ -6,3 +6,11 @@ cat = {
 
 cat['age'] # 2.3
 cat['isCute'] # True
+
+artist = {
+    "first": "Neil",
+    "last": "Young",
+}
+
+full_name = artist['first'] + ' ' + artist['last']
+print(full_name)
