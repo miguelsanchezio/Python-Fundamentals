95fd7bd3c64c59c07efe02d99df7315a894f5917
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-03 12:41:11 -0800

    Practiced list comprehension.

---
+++ Dictionaries/state_abbreviations_exercise.py
@@ -0,0 +1,5 @@
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {list1[num]: list2[num] for num in range(0, len(list1))}
