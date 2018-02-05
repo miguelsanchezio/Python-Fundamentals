1c70d1e964737c299f9aad27821a54b270561b00
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-05 11:34:51 -0800

    Practiced more dictionary comprehension.

---
+++ Dictionaries/list_to_dictionary_exercise.py
@@ -0,0 +1,4 @@
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+answer = {person[num][0]: person[num][1] for num in range(0, len(person))}
