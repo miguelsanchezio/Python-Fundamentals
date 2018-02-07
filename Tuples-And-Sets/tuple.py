2750c2626e4ddac800c884199b7952c90bb1a505
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-07 10:22:41 -0800

    Learned about tuples.

---
+++ Tuples-And-Sets/tuple.py
@@ -0,0 +1,12 @@
+# immutable
+months = ('January', 'February', 'March', 'April', 'May', 'June')
+february = months[1]
+
+# can be used as keys in dictionaries
+locations = {
+    (32.1412, 34.4123): 'Tokyo Office',
+    (12.5412, -12.4121): 'New York Office',
+    (31.4541, 12.4211): 'San Francisco Office'
+}
+
+# dictionary.items() returns tuples
