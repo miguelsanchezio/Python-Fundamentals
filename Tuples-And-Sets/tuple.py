114cb15df130e95138ea6d4c7c7b8a1df9299a4f
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-07 10:22:41 -0800

    Learned about tuple looping and methods.

--- Tuples-And-Sets/tuple.py
+++ Tuples-And-Sets/tuple.py
@@ -2,6 +2,24 @@
 months = ('January', 'February', 'March', 'April', 'May', 'June')
 february = months[1]
 
+for month in months:
+    print(month)
+
+i = len(months) - 1
+while i >= 0:
+    print(months[i])
+    i -= 1
+
+# tuples methods
+months.count('January') # 1
+months.index('March') # 2
+
+# tuples can be nested
+nums = (1, 2, 3, (4, 5), 6, 7)
+
+# tuples can be sliced
+nums[1:4] # (2, 3, (4, 5))
+
 # can be used as keys in dictionaries
 locations = {
     (32.1412, 34.4123): 'Tokyo Office',
