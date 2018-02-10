35cb8312ef0f1308da6cfa12cd309a8ce88ce576
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-10 07:53:51 -0800

    Learned about using parameters in functions in Python.

---
+++ Functions/parameters.py
@@ -0,0 +1,15 @@
+def square(num):
+    return num**2
+
+print(square(4))
+print(square(8))
+
+def sing_happy_bday(name):
+    print(f'Happy B-Day Dear {name}')
+
+sing_happy_bday('Roshiba')
+
+def print_full_name(first, last):
+    print(f'{first} {last}')
+
+print_full_name('Miguel', 'Sanchez')
