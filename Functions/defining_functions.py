b6d57e82e9f8d3a19f247157adbb988e7382d615
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-10 07:53:51 -0800

    Learned about returning functions.

--- Functions/defining_functions.py
+++ Functions/defining_functions.py
@@ -3,4 +3,4 @@ def sing_happy_bday():
     print('Happy B-Day To You')
     print('Happy B-Day To You')
 
-# sing_happy_bday()
+sing_happy_bday()
---
+++ Functions/returning_functions.py
@@ -0,0 +1,12 @@
+# def print_square_of_7():
+#     print(7**2)
+
+# print_square_of_7()
+
+def square_of_9():
+    return 9**2
+
+result = square_of_9()
+print(result)
+
+# return exits the function
