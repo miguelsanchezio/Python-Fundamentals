553f3607c9e2a738c61ea18620aef27741110e34
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-11 12:41:51 -0800

    Practiced using default parameters in functions.

--- Functions/default_parameters.py
+++ Functions/default_parameters.py
@@ -4,3 +4,15 @@ def exponent(num, power=2):
 print(exponent(2, 3)) # 8
 print(exponent(3)) # 9
 print(exponent(7)) # 49
+
+def speak(animal='dog'):
+    if animal == 'pig':
+        return 'oink'
+    elif animal == 'duck':
+        return 'quack'
+    elif animal == 'cat':
+        return 'meow'
+    elif animal == 'dog':
+        return 'woof'
+    else:
+        return '?'
