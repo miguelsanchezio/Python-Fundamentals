d81f5e6059fa41562f940e535219aeb144cdb47f
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-12 11:22:53 -0800

    Learned about docstrings.

---
+++ Functions/docstrings.py
@@ -0,0 +1,6 @@
+def exponent(num, power=2):
+    """exponent(num, power) raises num to specified power. Power defaults to 2."""
+    return num ** power
+
+print(exponent(2, 3)) # 8
+print(exponent.__doc__)
