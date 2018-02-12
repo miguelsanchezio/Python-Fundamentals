5e0011614e3e7d69bee4a7893148639be2f2132a
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-12 11:22:53 -0800

    Learned about global and nonlocal variables.

--- Functions/scope.py
+++ Functions/scope.py
@@ -3,5 +3,20 @@ def say_hello():
     return f'Hello {instructor}'
 
 say_hello()
-
 print(instructor) # NameError
+
+
+total = 0
+def increment():
+    global total += 1
+    return total
+
+increment()
+
+def outer():
+    count = 0
+    def inner():
+        nonlocal count
+        count += 1
+        return count
+    return inner()
