0473b50b62010c04fe8918a457aaabf8793ebc0c
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-13 15:25:11 -0800

    Practiced functions by writing single_letter_count() function.

---
+++ Functions/function-exercises/single_letter_count.py
@@ -0,0 +1,9 @@
+'''
+single_letter_count("Hello World", "h") # 1
+single_letter_count("Hello World", "z") # 0
+single_letter_count("HelLo World", "l") # 3
+'''
+
+#define single_letter_count below:
+def single_letter_count(string1, string2):
+    return string1.lower().count(string2.lower())
