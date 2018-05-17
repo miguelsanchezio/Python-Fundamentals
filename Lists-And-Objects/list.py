3eea10c86a98d8f0874e9765290814f41cbab348
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-17 11:23:12 -0700

    Learned about accessing data in lists in Python.

--- Lists-And-Objects/list.py
+++ Lists-And-Objects/list.py
@@ -1,6 +1,13 @@
 # lists
 demo_list = ['a', 1, 24, True, 5.123]
-print(len(demo_list))
+# print(len(demo_list))
 
 tasks = list(range(1, 4))
-print(tasks)
+# print(tasks)
+
+# accessing values in a list
+colors = ['red', 'blue', 'green']
+print(colors[0])
+print(colors[2])
+'orange' in colors # False
+'red' in colors # True
