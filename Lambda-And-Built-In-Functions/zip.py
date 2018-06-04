3eb4abc582b365c2c4aedf1f478dca65bd55c3e1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-06-04 08:55:24 -0700

    Learned about zip in Python.

--- Lambda-And-Built-In-Functions/zip.py
+++ Lambda-And-Built-In-Functions/zip.py
@@ -3,3 +3,23 @@ nums2 = [6,7,8,9,10]
 
 z = zip(nums1, nums2)
 list(z) # [(1,6),(2,7),(3,8),(4,9),(5,10)]
+
+midterms = [80,91,78]
+finals = [98, 89, 54]
+students = ['dan', 'ang', 'kate']
+
+# final_grades = [max(pair) for pair in zip(midterms, finals)]
+
+# final_grades ={t[0]:max(t[1], t[2]) for t in zip(students, midterms, finals)}
+
+final_grades = dict(
+    zip(
+        students,
+        map(
+            lambda pair: max(pair)
+            zip(midterms, finals)
+        )
+    )
+)
+
+# print(final_grades)
