e9abd5c6c73aa994f6cb2cfe3bae50daf6819aa2
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-06-07 12:31:16 -0700

    Practiced external modules by building an ascii art application.

---
+++ Modules/ascii_art_exercise.py
@@ -0,0 +1,15 @@
+import pyfiglet
+from termcolor import colored
+
+valid_colors = ('red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
+
+msg = input('What would you like to print?')
+color = input('What color?')
+
+if color not in valid_colors:
+    color = 'magenta'
+
+ascii_art = pyfiglet.figlet_format(msg)
+colored_ascii = colored(ascii_art, color=color)
+
+print(colored_ascii)
