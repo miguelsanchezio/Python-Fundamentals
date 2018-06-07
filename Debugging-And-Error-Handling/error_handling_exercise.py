42f23bc36b92a3cd12472506d106ce4242a62f33
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-06-06 23:31:34 -0700

    Practiced error handling in Python by writing a function.

---
+++ Debugging-And-Error-Handling/error_handling_exercise.py
@@ -0,0 +1,8 @@
+def divide(num1,num2):
+    try:
+        total = num1 / num2
+    except TypeError:
+        return 'Please provide two integers or floats'
+    except ZeroDivisionError:
+        return 'Please do not divide by zero'
+    return total
