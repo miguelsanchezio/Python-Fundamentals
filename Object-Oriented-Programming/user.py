fa5306d62009cbeec14d3b7215d921c3c615a170
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-03-31 15:28:20 -0700

    Learn about instance attributes and instance methods.

--- Object-Oriented-Programming/user.py
+++ Object-Oriented-Programming/user.py
@@ -4,9 +4,22 @@ class User:
         self.last = last
         self.age = age
 
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
+    def likes(self, thing):
+        return f"{self.first} likes {thing}."
+
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return f"Happy {self.age}th Birthday, {self.first}!"
+
 
 user1 = User("Joe", "Gutierrez", 28)
 user2 = User("Marilynn", "Sanchez", 24)
-
-print(user1.first, user1.last)
-print(user2.first, user2.last)
