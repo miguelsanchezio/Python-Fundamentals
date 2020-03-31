035d6a1e19bad64104886878aaeb259b428916a2
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-03-31 00:17:35 -0700

    Learn about classes and __init__(self)

--- Object-Oriented-Programming/user.py
+++ Object-Oriented-Programming/user.py
@@ -1,2 +1,12 @@
 class User:
-    pass
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+
+user1 = User("Joe", "Gutierrez", 28)
+user2 = User("Marilynn", "Sanchez", 24)
+
+print(user1.first, user1.last)
+print(user2.first, user2.last)
