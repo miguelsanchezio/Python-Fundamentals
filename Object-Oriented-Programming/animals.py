a75637be9da2a3b3fa1a47f05a3a9c689e43a43d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-01 21:02:08 -0700

    Learn about multiple inheritance

---
+++ Object-Oriented-Programming/animals.py
@@ -0,0 +1,25 @@
+class Aquatic:
+    def __init__(self, name):
+        self.name = name
+
+    def swim(self):
+        return f"{self.name} is swimming"
+
+    def greet(self):
+        return f"I am {self.name} of the sea!"
+
+
+class Ambulatory:
+    def __init__(self, name):
+        self.name = name
+
+    def walk(self):
+        return f"{self.name} is walking"
+
+    def greet(self):
+        return f"I am {self.name} of the land!"
+
+class Penguin(Ambulatory, Aquatic):
+    def __init__(self, name):
+        super().__init__(name=name)
+
