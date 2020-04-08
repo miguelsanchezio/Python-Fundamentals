5bf0f16707beeb63a4413a36668f9c4f430f6ada
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-07 21:31:45 -0700

    Create second make_song version

--- Iterators-And-Generators/make_song_exercise.py
+++ Iterators-And-Generators/make_song_exercise.py
@@ -1,4 +1,17 @@
 def make_song(count=99, beverage="soda"):
+    while count >= 0:
+        if count > 1:
+            i = count
+            count -= 1
+            yield "{} bottles of {} on the wall.".format(i, beverage)
+        elif count == 1:
+            count -= 1
+            yield "Only 1 bottle of {} left!".format(beverage)
+        elif count == 0:
+            count -= 1
+            yield "No more {}!".format(beverage)
+
+def make_song_2(count=99, beverage="soda"):
     for num in range(count, -1, -1):
         if num > 1:
             yield "{} bottles of {} on the wall.".format(num, beverage)
