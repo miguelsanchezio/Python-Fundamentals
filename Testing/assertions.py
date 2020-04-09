b330c5e19ebbe6065ce1fa159ed3962b0efaf921
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-09 09:09:51 -0700

    Learn about assertion in Python

---
+++ Testing/assertions.py
@@ -0,0 +1,10 @@
+def add_positive_numbers(x, y):
+    assert x > 0 and y > 0, "Both numbers must be positive."
+    return x + y
+
+
+add_positive_numbers(1, 1)
+add_positive_numbers(1, -1)
+
+# If a Python file is run with the -O flag,
+# assertions will not be evaluated
