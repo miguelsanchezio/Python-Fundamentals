b2ab2f50aba4a1faab940004d7f3f9965aa349b2
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-09 12:39:43 -0700

    Learn more about asserts in unit tests

--- Testing/activities.py
+++ Testing/activities.py
@@ -1,4 +1,9 @@
+from random import choice
+
+
 def eat(food, is_healthy):
+    if not isinstance(is_healthy, bool):
+        raise ValueError("is_healthy must be a boolean")
     ending = "because YOLO."
     if is_healthy:
         ending = "because my body is a temple."
@@ -9,3 +14,13 @@ def nap(num_hours):
     if num_hours >= 2:
         return f"Whoops! I took a long {num_hours} hours nap!"
     return f"I'm feeling refreshed after my {num_hours} hour nap."
+
+
+def is_funny(person):
+    if person == 'tim': return False
+    return True
+
+
+def laugh():
+    return choice(("lol", "lmao", "rofl"))
+
--- Testing/tests.py
+++ Testing/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from .activities import eat, nap
+from .activities import eat, nap, is_funny, laugh
 
 
 class ActivityTests(unittest.TestCase):
@@ -17,6 +17,10 @@ class ActivityTests(unittest.TestCase):
             "I'm eating pizza, because YOLO."
         )
 
+    def test_eat_healthy_boolean(self):
+        with self.assertRaises(ValueError):
+            eat("pizza", is_healthy="maybe")
+
     def test_short_nap(self):
         """nap should have a message for a short nap"""
         self.assertEqual(
@@ -31,6 +35,17 @@ class ActivityTests(unittest.TestCase):
             "Whoops! I took a long 3 hours nap!"
         )
 
+    def test_is_funny_time(self):
+        # self.assertEqual(is_funny("tim"), False)
+        self.assertFalse(is_funny("tim"), "Tim shouldn't be funny.")
+
+    def test_is_funny_anyone_else(self):
+        self.assertTrue(is_funny("blue"), "blue should be funny")
+        self.assertTrue(is_funny("tammy"), "blue should be tammy")
+        self.assertTrue(is_funny("sven"), "blue should be sven")
+
+    def test_laugh(self):
+        self.assertIn(laugh(), ("lol", "lmao", "rofl"))
 
 if __name__ == "__main__":
     unittest.main()
