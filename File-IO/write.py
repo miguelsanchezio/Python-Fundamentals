c20ab2b37e430c9ea61ccf43c5aa0fb91e23ab8c
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-09 18:09:57 -0700

    Learn how to use write()

---
+++ File-IO/write.py
@@ -0,0 +1,11 @@
+with open("haiku.txt", "w") as file:
+    file.write("Writing files is great\n")
+    file.write("Here's another line of text\n")
+    file.write("Closing now, goodbye!\n")
+    file.write("\n")
+
+with open("haiku.txt", "w") as file:
+    file.write("Here's one more haiku\n")
+    file.write("What about the older one?\n")
+    file.write("Let's go check it out\n")
+    file.write("\n")
