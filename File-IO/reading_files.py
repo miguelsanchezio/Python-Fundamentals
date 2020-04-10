1a56a32d909a1cc0892f6469b4ce2c8a4fec7158
Miguel Sanchez <msanchez.anaheim@gmail.com>
2020-04-09 17:58:15 -0700

    Learn about seek(), readline(), readlines(), and close()

--- File-IO/reading_files.py
+++ File-IO/reading_files.py
@@ -1,2 +1,30 @@
 f = open("story.txt")
+
+# reads from start to finish, moves cursor to the end of file
+f.read()
+
+# moves the cursor
+f.seek(0)
+
 print(f.read())
+
+f.seek(0)
+
+# reads one line at a time
+f.readline()
+
+f.seek(0)
+
+# returns a list of lines
+f.readlines()
+
+# closes the file
+f.close()
+
+# returns true of file is closed, false otherwise
+f.closed
+
+# opens the file and then closes the file after the block is resolved
+with open("story.txt") as file:
+    data = file.read()
+
